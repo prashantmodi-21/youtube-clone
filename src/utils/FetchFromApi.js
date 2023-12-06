a86a342912900b39633e74f6395a92f9a144716d
unknown <imprashantmodi21@gmail.com>
2023-12-06 10:11:01 +0530

    Fix Exposed Api Keys

--- src/utils/FetchFromApi.js
+++ src/utils/FetchFromApi.js
@@ -4,7 +4,7 @@ const options = {params: {
     maxResults: '50'
   },
   headers: {
-    'X-RapidAPI-Key': 'e6b18c4d91msh07a867471a62da8p144295jsn99e08db295e7',
+    'X-RapidAPI-Key':  import.meta.env.VITE_YT_CLONE_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }}
 export const FetchApi = async(query)=>{
